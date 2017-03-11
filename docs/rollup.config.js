42007774665dce8587abf8263da92716b736d957
Denis <carriere.denis@gmail.com>
2017-03-11 00:07:45 -0500

    Publish commonjs

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
   entry: 'index.js',
   dest: 'docs/global-mercator.min.js',
   format: 'umd',
+  plugins: [
+    commonjs()
+  ],
+  useStrict: false,
   moduleName: 'mercator'
 }
