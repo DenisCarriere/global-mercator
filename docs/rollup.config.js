cebe9f3638cad0717bc7590077494a2b8fc9fa04
Denis <carriere.denis@gmail.com>
2017-03-10 23:56:39 -0500

    Fix rollup build

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,20 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import builtins from 'rollup-plugin-node-builtins'
-import globals from 'rollup-plugin-node-globals'
-import json from 'rollup-plugin-json'
-
 module.exports = {
   entry: 'index.js',
   dest: 'docs/global-mercator.min.js',
   format: 'umd',
-  plugins: [
-    json(),
-    resolve(),
-    commonjs(),
-    globals(),
-    builtins()
-  ],
-  useStrict: false,
   moduleName: 'mercator'
 }
